7b3996df6ae9b7294cbdaaaeea6d4959f7a9ec77
elgonziiiii <gonzalo.valdezg@uanl.edu.mx>
2024-09-12 15:33:20 -0600

    Update script.pia.ps1

--- script.pia.ps1
+++ script.pia.ps1
@@ -1,25 +1,55 @@
+Set-StrictMode -Version Latest
+
 function get-menu {
     do {
-    $menu = Read-Host " `nPlease choose an option:`n 1: Listado de Archivos`n 2: Uso de Recursos`n 3: Tarea Adicional`n 4: Exit`n Opcion"
-    switch ($menu) {
-        1{ 
-            Write-Host "listado de Archivos"
-            #modulo
+        try {
+           
+            $menu = Read-Host " `nPlease choose an option:`n 1: Listado de Archivos`n 2: Uso de Recursos`n 3: Tarea Adicional`n 4: Exit`n Opcion"
+            
+            switch ($menu) {
+                1{ 
+                    try {
+                        Write-Host "Listado de Archivos"
+                        
+                    } catch {
+                        Write-Host "Error al listar los archivos: $_"
+                    }
+                }
+                2{
+                    try {
+                        Write-Host "Uso de Recursos"
+                        
+                    } catch {
+                        Write-Host "Error al mostrar el uso de recursos: $_"
+                    }
+                }
+                3{
+                    try {
+                        Write-Host "Tarea Adicional"
+                       
+                    } catch {
+                        Write-Host "Error en la tarea adicional: $_"
+                    }
+                }
+                4{
+                    Write-Host "Saliendo..."
+                    exit
+                }
+                default {
+                    Write-Host "Opción no válida. Por favor, elige una opción del 1 al 4."
+                }
+            }
+        } catch {
+            Write-Host "Error general: $_"
+        } finally {
+            
+            Write-Host "Operacion Finalizada"
         }
-        2{
-            Write-Host "Uso de Recursos"
-            #modulo
-        }
-        3{
-            Write-Host "Tarea Adicional"
-            #modulo
-        }
-        4{
-            Write-Host "leaving... "
-            exit
-        }
-    }
-    } while ($true)}
+    } while ($true)
+}
+
+
+
 
 
 
