4f7b40ad50de2d37e570ccb6f4d65e1a0a0e21a0
Josa9 <92348764+dantedm7911@users.noreply.github.com>
2024-09-18 01:10:22 -0600

    API_VIRUSTOTAL_2.psm1

--- API_VIRUSTOTAL_2.psm1
+++ API_VIRUSTOTAL_2.psm1
@@ -1,3 +1,31 @@
+
+<#
+.SYNOPSIS
+Genera una lista de hashes SHA-256 de archivos en el directorio actual y consulta la API de VirusTotal para analizar esos hashes.
+
+.DESCRIPTION
+La función `Api_HashView` realiza los siguientes pasos:
+1. Obtiene todos los archivos en el directorio actual y calcula el hash SHA-256 de cada uno.
+2. Exporta los hashes a un archivo CSV.
+3. Importa los hashes del archivo CSV.
+4. Consulta la API de VirusTotal para cada hash utilizando una clave API proporcionada.
+5. Muestra el nombre del archivo y los resultados del análisis (malicioso y sospechoso) en la consola.
+6. Hace una pausa de 15 segundos entre cada consulta para evitar exceder el límite de solicitud de la API.
+
+.EXAMPLE
+Api_HashView
+Genera hashes SHA-256 para todos los archivos en el directorio actual, exporta estos hashes a un archivo CSV y luego consulta la API de VirusTotal para obtener información sobre cada hash.
+
+.PARAMETER
+Este script no acepta parámetros.
+
+.NOTES
+- Asegúrate de tener una clave API válida de VirusTotal.
+- La función realiza una pausa de 15 segundos entre cada consulta para evitar exceder los límites de solicitud de la API.
+- Los resultados de la consulta se muestran en la consola.
+
+#>
+
 function Api_HashView {
   Set-StrictMode -Version Latest
     Write-Host "Generaremos una lista de hashes para registrar su sistema de archivos local"
