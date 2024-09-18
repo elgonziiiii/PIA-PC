f7549aeb787fcf2ec93acdf4f3fb11665ae84b8f
Josa9 <92348764+dantedm7911@users.noreply.github.com>
2024-09-17 23:17:43 -0600

    Recursos.psm1

--- Recursos.psm1
+++ Recursos.psm1
@@ -1,11 +1,32 @@
+#JOSAFAT DE LA GARZA AMARO 2076405
+#CODIGO PARA VER EL USO DE LA CPU Y LA RAM Y DISCO DURO 
+function Resource-Usage{
+
 $totalRam = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).Sum
-while($true) {
+while ($true) {
     $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    
+    # Las siguientes 3 lineas de codigo se usan para encontrar el valor de los recursos que se estan usando 
     $cpuTime = (Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
+    
     $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
-    $date + ' > CPU: ' + $cpuTime.ToString("#,0.000") + '%, Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%)'
+    
+    $diskFree = (Get-Counter '\LogicalDisk(_Total)\% Free Space').CounterSamples.CookedValue
+    
+    # Aqui se muestran los resultados, por favor no moverle a nada 
+    $output = "$date > CPU: {0}%, Avail. RAM.: {1}MB ({2}%), Disco Libre: {3}%" -f `
+        $cpuTime.ToString("#,0.000"), `
+        $availMem.ToString("N0"), `
+        (104857600 * $availMem / $totalRam).ToString("#,0.0"), `
+        $diskFree.ToString("#,0.0")
+    
+    Write-Output $output
+    
     Start-Sleep -s 2
-} 
+ } RAM 
+
+}
 
 
-#CODIGO PARA VER EL USO DE LA CPU Y LA RAM 
+
+#CODIGO PARA VER EL USO DE LA CPU Y LA RAM Y DISCO DURO 
