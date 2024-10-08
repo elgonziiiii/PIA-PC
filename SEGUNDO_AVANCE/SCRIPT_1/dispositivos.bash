2fe02e2449e9b0aedd8be18ea92236ed879dd5f2
Josa9 <92348764+dantedm7911@users.noreply.github.com>
2024-10-07 22:27:02 -0600

    Update dispositivos.bash

--- SEGUNDO_AVANCE/SCRIPT_1/dispositivos.bash
+++ SEGUNDO_AVANCE/SCRIPT_1/dispositivos.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
- 
+
+escaneo(){
+    
 # Inicializa una lista para los hosts activos
 hosts_activos=()
  
 # Bucle para escanear los hosts
 for i in {1..255}; do
-    if timeout 2 ping -c 1 127.0.0.$i >/dev/null 2>&1; then
-        echo "El host 127.0.0.$i esta activo"
+    if timeout 2 ping -c 1 192.168.56.$i >/dev/null 2>&1; then
+        echo "El host 192.168.56.$i está activo"
         # Agrega el host a la lista de hosts activos
-        hosts_activos+=("127.0.0.$i")
+        hosts_activos+=("192.168.56.$i")
     fi
 done
  
@@ -17,3 +19,4 @@ echo -e "\nDispositivos conectados a la red local:"
 for host in "${hosts_activos[@]}"; do
     echo "$host"
 done
+}
