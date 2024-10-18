6904a055ae0ac4df5bf57000b4ac6655ceb79740
Josa9 <92348764+dantedm7911@users.noreply.github.com>
2024-10-18 02:54:13 -0600

    Update modulo1.py

--- TERCER_AVANCE/modulo1.py
+++ TERCER_AVANCE/modulo1.py
@@ -2,6 +2,8 @@ import string
 import random
 
 def generar_contrasena(longitud):
+
+    #manejo de errores en caso de que el usuario introduzca un valor igual o menor a cero
     try:
         if longitud <= 0:
             raise ValueError("La longitud debe ser un número positivo.")
@@ -10,4 +12,4 @@ def generar_contrasena(longitud):
         return ''.join(random.choice(caracteres) for _ in range(longitud))
     
     except Exception as e:
-        return f"Ocurrió un error: {e}"
+        return f"Volver a seleccionar opcion 1 Ocurrió un error: {e}"
