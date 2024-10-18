6518ab5e05272e3f2c95b939c6968cd93144bf8c
Josa9 <92348764+dantedm7911@users.noreply.github.com>
2024-10-18 00:47:04 -0600

    Update modulo5.py

--- TERCER_AVANCE/modulo5.py
+++ TERCER_AVANCE/modulo5.py
@@ -2,32 +2,34 @@ import requests
 
 API_KEY = '5898a9654424864f1ffb3635fa985232386867b66acf4a71c5376c3eb617ae59d605a8de292562e7'
 
-ip_address = str(input("Ingresa la direccion IP que quieras analizar: "))   # Dirección IP que deseasverificar
-
-url = f'https://api.abuseipdb.com/api/v2/check'  # URL de la API
-
-params = {
-    'ipAddress': ip_address,     # Parámetros de la solicitud
-    'maxAgeInDays': 90       
-}
-
-headers = {
-    'Accept': 'application/json',
-    'Key': API_KEY
-}
-
-
-response = requests.get(url, headers=headers, params=params) # Se hace la solicitud
-
-if response.status_code == 200:  # Verifica si hay respuesta
+def analizar_ip_abuse(ip_address):
     
-    data = response.json()  # Procesa la respuesta
-    print("Información sobre la IP:")
-    print(f"Dirección IP: {data['data']['ipAddress']}")
-    print(f"País: {data['data']['countryCode']}")
-    print(f"Reportes: {data['data']['totalReports']}")
-    print(f"Última actividad maliciosa: {data['data']['lastReportedAt']}")
-    print(f"Score de abuso: {data['data']['abuseConfidenceScore']}")
+    url = f'https://api.abuseipdb.com/api/v2/check'  # URL de la API
+
+    params = {
+        'ipAddress': ip_address,     # Parámetros de la solicitud
+        'maxAgeInDays': 90       
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Key': API_KEY
+    }
+
+
+    response = requests.get(url, headers=headers, params=params) # Se hace la solicitud
+
+    if response.status_code == 200:  # Verifica si hay respuesta
+    
+        data = response.json()  # Procesa la respuesta
+        result = (
+           "Información sobre la IP:\n"
+           f"Dirección IP: {data['data']['ipAddress']}\n"
+           f"País: {data['data']['countryCode']}\n"
+           f"Reportes: {data['data']['totalReports']}\n"
+           f"Última actividad maliciosa: {data['data']['lastReportedAt']}\n"
+           f"Score de abuso: {data['data']['abuseConfidenceScore']}\n"
+        )
+        return result
 else:
-    print(f"Error en la solicitud: {response.status_code}")
-    print(response.text)
+    return f"Error en la solicitud: {response.status_code}\n{response.text}"
